5efccc9c42525deeeebb0cac4142bb822cee95cd
Cicilini <guilherme.cicilini@hotmail.com>
2024-04-09 23:09:31 -0300

    configuration prettier and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express, { json} from 'express'
+import express, { json } from 'express';
 
-const app = express()
+const app = express();
 
-app.use(json())
+app.use(json());
 
-app.listen(3333, () => console.log('🚀 running port 3333!'))
+app.listen(3333, () => console.log('🚀 running port 3333!'));
