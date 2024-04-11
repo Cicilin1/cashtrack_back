5332e276ea7c3c104962b69255e7b354d05c489c
Cicilini <guilherme.cicilini@hotmail.com>
2024-04-11 11:27:33 -0300

    feat: entities

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,26 @@
+
+type categoryProps = {
+    title: string,
+    id?: string,
+    color: string
+}
+
+export class Category {
+    public id?: string;
+    public title: string;
+    public color: string;
+
+    constructor({title, id, color }: categoryProps ) {
+        this.id = id;
+        this.title = title;
+        this.color  = color;
+    }
+
+}
+
+
+const category = new Category({
+    title: 'title',
+    id: 'salve',
+    color: 'red'
+})
