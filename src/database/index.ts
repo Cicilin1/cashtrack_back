3cce58d1c84822283617ed2f3163516c45d10879
Cicilini <guilherme.cicilini@hotmail.com>
2024-04-17 22:20:23 -0300

    connetion to database

---
+++ src/database/index.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+export async function setupMongo(): Promise<void> {
+    try {
+        if (mongoose.connection.readyState == 1) {
+            return;
+        }
+
+        console.log('📊 Connectin to DB...');
+        await mongoose.connect(process.env.MONGO_URL as string)
+        console.log('🟢 DB Connected!.')
+    } catch {
+        throw new Error('❌ DB not connected.');
+
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
+import 'dotend/config'
 import express, { json } from 'express';
+
+import { setupMongo } from './database';
 import { routes } from './routes';
 
 
+setupMongo().then(() => {
+    const app = express();
 
-const app = express();
+    app.use(json());
+    app.use(routes)
 
-app.use(json());
-app.use(routes)
+    app.listen(3333, () => console.log('🚀 running port 3333!'));
 
-app.listen(3333, () => console.log('🚀 running port 3333!'));
+})
+
+
+
